78b42f6197fb7247f5fc6605c8323d34ae0d55c0
Dacrido <125836065+Dacrido@users.noreply.github.com>
2023-05-18 20:16:15 -0400

    MC Finished.
    First enemies to be expected soon (2/3 days)

--- VideoGame/Assets/Scripts/Enemy/Temp_MC/Temp_Projectile.cs
+++ VideoGame/Assets/Scripts/Enemy/Temp_MC/Temp_Projectile.cs
@@ -2,17 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Weapon : MonoBehaviour
+public class Temp_Projectile : MonoBehaviour
 {
+
+    [SerializeField] private Rigidbody2D rb;
+    private float speed = 600.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb.AddForce(transform.right * speed);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
     }
 }
