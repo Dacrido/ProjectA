f02c07336fc5a40e9d3874dc061e2f41ff3c59a8
Dacrido <125836065+Dacrido@users.noreply.github.com>
2023-05-22 20:15:30 -0400

    Improving Health system
    Final thing needed: short invincible period after taking any amount of damage.

--- VideoGame/Assets/Scripts/Enemy/Health/Health.cs
+++ VideoGame/Assets/Scripts/Enemy/Health/Health.cs
@@ -1,10 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Health : MonoBehaviour
 {
-
+    private Slider HealthBar;
     public int maxHealth;
     [HideInInspector] public int currentHealth;
 
@@ -12,22 +13,44 @@ public class Health : MonoBehaviour
     void Start() // This is current health for enemies. When activated/spawned, whenever entering a new area, they start with max health
     {
         currentHealth = maxHealth;
+
+        if (gameObject.tag == "Player") { return; }
+        HealthBar = GetComponentInChildren<Slider>();
+        HealthBar.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
-    {
+    {   
+        if (gameObject.tag == "Player") { return; }
+
+        if (HealthBar.gameObject.activeSelf && currentHealth == maxHealth) // If the health bar is active and the currentHealth is equal to the maxHealth, hide health bar after 3 seconds
+        {
+            StartCoroutine(Wait(2));
+            HealthBar.gameObject.SetActive(false);
+        }
+
+        if (!HealthBar.gameObject.activeSelf && currentHealth != maxHealth)
+        {
+            HealthBar.gameObject.SetActive(true);
+        }
         
     }
 
-    public void takeDamage(int damage)
+    IEnumerator Wait(int seconds)
     {
+        yield return new WaitForSeconds(seconds);
+    }
+
+    public void takeDamage(int damage)
+    {   
         currentHealth -= damage;
 
         if (currentHealth <= 0)
         {
             Destroy(gameObject);
         }
+
     }
     
     public void healHealth(int health)
