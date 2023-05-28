5d5393a320214b927e93de02861dcf12e5479941
Dacrido <125836065+Dacrido@users.noreply.github.com>
2023-05-28 14:27:41 -0400

    Some Code Organization

--- VideoGame/Assets/Scripts/Enemy/Movement/Default/Enemy_Default_Walking.cs
+++ VideoGame/Assets/Scripts/Enemy/Movement/Default/Enemy_Default_Walking.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.Http.Headers;
 using UnityEditor.Tilemaps;
 using UnityEngine;
 
@@ -15,93 +16,28 @@ public class Enemy_Default_Walking : MonoBehaviour
 
 
     private Rigidbody2D rb;
-    
+    private Enemy_General_Movement_Ground General;
 
     [SerializeField] private float speed;
-    int direction; // The only possible directions are left and right (-1 and 1) ****** The whole directions system will be reworked later ****************
-
-    LayerMask groundLayer;
-    private BoxCollider2D boxCollider;
-
-    int enemyLayer; // ***** This will be placed in another script later. One script for movement that all enemies will have, followed by various scripts like walking, hopping, etc...
 
     // Start is called before the first frame update    
     void Start()
     {
-        enemyLayer = LayerMask.NameToLayer("Enemy");        
-        Physics2D.IgnoreLayerCollision(enemyLayer, enemyLayer); // Prevents collisions between enemies
-
-        groundLayer = LayerMask.GetMask("Ground");
-
         rb = GetComponent<Rigidbody2D>();
-        boxCollider = GetComponent<BoxCollider2D>();
+        General = GetComponent<Enemy_General_Movement_Ground>();
 
-        switch (Random.Range(1, 3))
-        {
-            case 1: direction = 1; break;
-            case 2: direction = -1; break;
-        }
-
-        
     }
 
     
     void Update()
     {
-        //RaycastHit2D forward = Physics2D.Raycast(transform.position, forward, distance, groundLayer); // This raycast checks in front of the enemy. In this case, only for groundLayer, but can be changed to also check for player
-
-        if (!isGrounded())
-        {
-            Flip();
-        } else if (isWalled())
-        {
-            Flip();
-        }
-
-    }
-    bool isGrounded()
-    {
-        // Getting position of downwards ray
-        Vector2 ray_Position = transform.position;
-        ray_Position.x += direction * (boxCollider.offset.x + boxCollider.size.x / 2); // Places the x position to the front of the enemy depending on direction
-        ray_Position.y -= boxCollider.offset.y + boxCollider.size.y / 2;
-
-        // Direction and distance of downwards ray
-        Vector2 ray_Direction = Vector2.down;
-        float ray_Distance = 0.1f;
-
-        RaycastHit2D checkForGround = Physics2D.Raycast(ray_Position, ray_Direction, ray_Distance, groundLayer);
-
-        if (checkForGround.collider != null)
-            return true;
-        return false;
-
+        General.DefaultReaction(0.0f);
     }
 
-    bool isWalled()
-    {
-        Vector2 ray_Position = transform.position;
-        ray_Position.x += direction * (boxCollider.offset.x + boxCollider.size.x / 2);
-
-        Vector2 ray_Direction = direction * Vector2.right;
-        float ray_Distance = 0.05f;
-
-        Vector2 ray_Size = new Vector2 (0.05f, boxCollider.size.y);
-
-        RaycastHit2D checkForWall = Physics2D.BoxCast(ray_Position, ray_Size, 0.0f, ray_Direction, ray_Distance, groundLayer); // Box cast so that an obstacle at any height compared to the enemy is detected
-
-        if (checkForWall.collider != null)
-            return true;
-        return false;
-    }
-
-    void Flip()
-    {
-        direction *= -1;
-    }
+    
 
     void FixedUpdate()
     {
-        rb.velocity = new Vector2(direction * speed, rb.velocity.y);
+        rb.velocity = new Vector2(General.GetDirection() * speed, rb.velocity.y);
     }
 }
---
+++ VideoGame/Assets/Scripts/Enemy/Movement/Enemy_General_Movement_Ground.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_General_Movement_Ground : MonoBehaviour
+{
+
+    // This class contains general methods used by all the movement scripts. Most notably, raycasts, boolean methods and flip()
+
+    private int direction; // The only possible directions are left and right (-1 and 1) ****** The whole directions system will be reworked later ****************
+
+    LayerMask groundLayer;
+    private BoxCollider2D boxCollider;
+    
+    int enemyLayer;
+
+    // Start is called before the first frame update    
+    void Start()
+    {
+        enemyLayer = LayerMask.NameToLayer("Enemy");
+        Physics2D.IgnoreLayerCollision(enemyLayer, enemyLayer); // Prevents collisions between enemies (Might be changed later)
+
+        groundLayer = LayerMask.GetMask("Ground");
+
+        boxCollider = GetComponent<BoxCollider2D>();
+
+        switch (Random.Range(1, 3))
+        {
+            case 1: direction = 1; break;
+            case 2: direction = -1; break;
+        }
+
+    }
+
+    // extraRayDistance: extra distance added to the ray say if the enemy is jumping off the ground
+    public bool isGrounded(float extraRayDistance) // Checks if the enemy is on the ground of not
+    {
+        // Getting position of downwards ray
+        Vector2 ray_Position = transform.position;
+        ray_Position.x += direction * (boxCollider.offset.x + boxCollider.size.x / 2); // Places the x position to the front of the enemy depending on direction
+        ray_Position.y -= boxCollider.offset.y + boxCollider.size.y / 2;
+
+        // Direction and distance of downwards ray
+        Vector2 ray_Direction = Vector2.down;
+        float ray_Distance = 0.1f + extraRayDistance;
+
+        RaycastHit2D checkForGround = Physics2D.Raycast(ray_Position, ray_Direction, ray_Distance, groundLayer);
+
+        if (checkForGround.collider != null)
+            return true;
+        return false;
+    }
+
+    public bool isWalled()
+    {
+        Vector2 ray_Position = transform.position;
+        ray_Position.x += direction * (boxCollider.offset.x + boxCollider.size.x / 2);
+
+        Vector2 ray_Direction = direction * Vector2.right;
+        float ray_Distance = 0.05f;
+
+        Vector2 ray_Size = new Vector2(0.05f, boxCollider.size.y);
+
+        RaycastHit2D checkForWall = Physics2D.BoxCast(ray_Position, ray_Size, 0.0f, ray_Direction, ray_Distance, groundLayer); // Box cast so that an obstacle at any height compared to the enemy is detected
+
+        if (checkForWall.collider != null)
+            return true;
+        return false;
+    }
+
+    public void Flip()
+    {
+        direction *= -1;
+    }
+
+    public void DefaultReaction(float extra)
+    {
+        if (!isGrounded(extra))
+        {
+            Flip();
+        }
+        else if (isWalled())
+        {
+            Flip();
+        }
+    }
+
+    public int GetDirection()
+    {
+        return direction;
+    }
+}
